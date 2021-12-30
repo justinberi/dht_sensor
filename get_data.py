7f32d6bdf33d2d97bf35473745cc9970ad4b5715
Justin Beri <beri.j90@gmail.com>
2021-12-30 23:17:25 +0000

    data_script

--- get_data.py
+++ get_data.py
@@ -4,6 +4,9 @@ import Adafruit_DHT
 import datetime
 import time
 
+#import wandb
+#wandb.init(project="ceiling_temperature")
+
 # Sensor should be set to Adafruit_DHT.DHT11,
 # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 sensor = Adafruit_DHT.DHT22
@@ -22,16 +25,15 @@ def get_readings(pin):
 	# If this happens try again!
 	if humidity is not None and temperature is not None:
 		now = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-		data = '{date},{pin},{temp:0.3f},{hum:0.3f}'.format(date=now, pin=pin, temp=temperature, hum=humidity)
-		return data
+		return now,pin,temperature,humidity
 	else:
 		print('Failed to get reading. Try again!')
-		return ""
+		return None
 
 # Check if data exists if it does then append else headers
 from pathlib import Path
 
-my_file = Path("data.csv")
+my_file = Path("/home/pi/dht_sensor/data.csv")
 if not my_file.is_file():
 	with open(my_file, "w") as f:
 		f.write("date,pin,temperature,humidity\n")
@@ -39,7 +41,16 @@ if not my_file.is_file():
 while True:
 	for p in pins:
 		data = get_readings(p)
+
+#		d = {}
+#		d["time"] = data[0]
+#		d["temperature_"+str(data[1])] = data[2]
+#		d["humidity_"+str(data[1])] = data[3]
+#		wandb.log(d)
+
 		with open(my_file, "a") as f:
-			f.write(data+"\n")
+			strdata = '{},{},{},{}'.format(*data)
+			f.write(strdata+"\n")
 		time.sleep(0.1)
 	time.sleep(60*30)
+#	time.sleep(1)
